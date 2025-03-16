c9881cb76a0f85f7795acf0f7fd39646765a0803
Meera <meera.patel2984@gmail.com>
2025-03-16 18:54:35 +0000

    handle spanish

--- hello.go
+++ hello.go
@@ -3,16 +3,22 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishPrefix = "Hola, "
+const spanish = "Spanish"
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 
 	if name == "" {
 		name = "World"
+	}
+
+	if language == spanish {
+		return spanishPrefix + name
 	}
 
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -2,28 +2,36 @@ package main
 
 import "testing"
 
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
 func TestHello(t *testing.T) {
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Meera")
+		got := Hello("Meera", "")
 		want := "Hello, Meera"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("defaults to world when no name given", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 
 		want := "Hello, World"
 
 		assertCorrectMessage(t, got, want)
 
 	})
-}
+	t.Run("says hello in given language", func(t *testing.T) {
+		got := Hello("Meera", "Spanish")
 
-func assertCorrectMessage(t testing.TB, got, want string) {
-	t.Helper()
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		want := "Hola, Meera"
+
+		assertCorrectMessage(t, got, want)
+
+	})
 }
