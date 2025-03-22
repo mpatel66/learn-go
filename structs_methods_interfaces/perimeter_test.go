07e514ea64fa39913caab5c5d29e12322cd5a2b6
Meera <meera.patel2984@gmail.com>
2025-03-22 16:11:49 +0000

    calculate area of rectangle

--- structs_methods_interfaces/perimeter_test.go
+++ structs_methods_interfaces/perimeter_test.go
@@ -2,10 +2,20 @@ package structsmethodsinterfaces
 
 import "testing"
 
-func TestPeri(t *testing.T) {
-	t.Run("returns perimeter of shape", func(t *testing.T) {
+func TestRectangle(t *testing.T) {
+	t.Run("returns perimeter of rectangle", func(t *testing.T) {
 		got := Perimeter(10.0, 10.0)
 		want := 40.0
+
+		if got != want {
+			t.Errorf("got %.2f, want %.2f", got, want)
+		}
+
+	})
+
+	t.Run("returns area of rectangle", func(t *testing.T) {
+		got := Area(5.0, 10.0)
+		want := 50.0
 
 		if got != want {
 			t.Errorf("got %.2f, want %.2f", got, want)
