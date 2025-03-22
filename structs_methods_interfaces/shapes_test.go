03c3d421ac4663afd6f52bdee339b0c7014c4670
Meera <meera.patel2984@gmail.com>
2025-03-22 16:23:06 +0000

    use methods to calculate area

--- structs_methods_interfaces/shapes_test.go
+++ structs_methods_interfaces/shapes_test.go
@@ -16,12 +16,23 @@ func TestRectangle(t *testing.T) {
 
 	t.Run("returns area of rectangle", func(t *testing.T) {
 		rectangle := Rectangle{5.0, 10.0}
-		got := Area(rectangle)
+		got := rectangle.Area()
 		want := 50.0
 
 		if got != want {
 			t.Errorf("got %.2f, want %.2f", got, want)
 		}
 
+	})
+
+	t.Run("returns are of circle", func(t *testing.T) {
+		circle := Circle{10}
+
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g, want %g", got, want)
+		}
 	})
 }
